03bef0983e3156b9f44f173f1bbbb82bb8d15f36
Brian Fontaine <brian02440@live.fr>
2020-06-05 12:22:12 +0200

    exo5

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+    include'../header.php';
+?>
+<body>
+    <h1>Exercices 5</h1>
+    <p><?=$number ;$number;?><p>
+    <p><?=$number = 12 ;$number;?><p>
+</body>
+<?php
+    include'../footer.php';
+?>
